58af9a70bcd8f6724ef621a4d6221765243fd792
Adam <adamrayschmidt@gmail.com>
2024-03-01 20:42:43 -0800

    Add ability to add task and save it as an object.

--- scripts/script.js
+++ scripts/script.js
@@ -3,3 +3,25 @@ function changeColor(select) {
 
     select.style.color = selectedOption.style.color;
 }
+
+function saveTask() {
+    console.log("Saving...");
+
+    const name = $("#inputName").val();
+    const assignee = $("#inputAssignee").val();
+    const dueDate = $("#inputDueDate").val();
+    const section = $("#inputSection").val();
+    const priority = $("#inputPriority").val();
+    const status = $("#inputStatus").val();
+    const budget = $("#inputBudget").val();
+    const description = $("#inputDescription").val();
+
+    let x = new Task(name, assignee, dueDate, section, priority, status, budget, description);
+    console.log(x);
+}
+
+function init() {
+    console.log("This is a task manager");
+}
+
+window.onload = init;
---
+++ scripts/task.js
@@ -0,0 +1,12 @@
+class Task {
+    constructor (name, assignee, dueDate, section, priority, status, budget, description) {
+        this.name = name;
+        this.assignee = assignee;
+        this.dueDate = dueDate;
+        this.section = section;
+        this.priority = priority;
+        this.status = status;
+        this.budget = budget;
+        this.description = description;
+    }
+}
